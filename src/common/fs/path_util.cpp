106096c55ec040531098c699a67406bb9c1b6fa8
ReiKatari <45316432+ReiKatari@users.noreply.github.com>
2026-09-04 19:53:32 +0300

    fix(android): resolve layout attributes, use ic_build icon and use recursive CreateDirs for paths

--- src/common/fs/path_util.cpp
+++ src/common/fs/path_util.cpp
@@ -329,7 +329,7 @@ std::string GetLegacyPathString(EmuPath legacy_path) {
 void SetEdenPath(EdenPath eden_path, const fs::path& new_path) {
     auto& instance = PathManagerImpl::GetInstance();
     if (!FS::Exists(new_path)) {
-        FS::CreateDir(new_path);
+        FS::CreateDirs(new_path);
     }
     if (FS::IsDir(new_path)) {
         instance.SetEdenPathImpl(eden_path, new_path);
